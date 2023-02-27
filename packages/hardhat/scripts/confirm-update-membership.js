0758121a87193f4464c9f76be12a3936b9203cff
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-02-27 18:49:31 +0400

    Track Hardhat

---
+++ packages/hardhat/scripts/confirm-update-membership.js
@@ -0,0 +1,25 @@
+//npx hardhat run --network alfajores scripts/mint-safe.js
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    // We get the contract to deploy
+    const Membership = await hre.ethers.getContractFactory("Membership");
+    const membership = await Membership.attach(
+        "0x51E2AfB7aE86E407eC406bf199182fe7B44956b9"
+    );
+
+    await membership.updateMembership(0);
+
+    const tokenUri = await membership.tokenURI(0);
+
+    console.table({ tokenUri });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
